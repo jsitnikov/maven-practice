10cf68a5fb6b972c3bee63505166faa8207662d5
Jurij Sitnikov <jurij.sitnikov@gmail.com>
2018-08-23 21:34:20 +0300

    Formatted XML, increased JUnit version.

--- maven-practice-app/src/main/java/lt/sitnikov/maven/App.java
+++ maven-practice-app/src/main/java/lt/sitnikov/maven/App.java
@@ -1,20 +1,14 @@
 package lt.sitnikov.maven;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.text.MessageFormat;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.google.gson.Gson;
-
 public class App {
 
-	private static final Logger logger = LoggerFactory.getLogger(App.class);
-	
-	public static void main(String[] args) {
-	
-	}
-	
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
+
+    public static void main(String[] args) {
+
+    }
+
 }
