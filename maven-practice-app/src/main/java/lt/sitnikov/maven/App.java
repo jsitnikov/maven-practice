1af5b0f6f2853b1560c56edf17a599efc0780cd4
Jurij Sitnikov <jurij.sitnikov@gmail.com>
2018-10-23 20:17:19 +0300

    Use different properties based on profile.

--- maven-practice-app/src/main/java/lt/sitnikov/maven/App.java
+++ maven-practice-app/src/main/java/lt/sitnikov/maven/App.java
@@ -8,7 +8,7 @@ public class App {
     private static final Logger logger = LoggerFactory.getLogger(App.class);
 
     public static void main(String[] args) {
-
+        logger.debug("Application started.");
     }
 
 }
