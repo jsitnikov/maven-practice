0fe306f9139972d4690054e05bd68312a818c755
Jurij Sitnikov <jurij.sitnikov@gmail.com>
2018-11-13 20:28:52 +0200

    Make a clean-up.

--- maven-practice-app/src/main/java/lt/sitnikov/maven/App.java
+++ maven-practice-app/src/main/java/lt/sitnikov/maven/App.java
@@ -1,14 +1,9 @@
 package lt.sitnikov.maven;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class App {
 
-    private static final Logger logger = LoggerFactory.getLogger(App.class);
 
     public static void main(String[] args) {
-        logger.debug("Application started.");
     }
 
 }
